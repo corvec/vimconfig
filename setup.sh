10ad9787cb85099c096e1b53855c1f643b402bb5
Corey Kump <Corey.Kump@gmail.com>
2014-02-07 15:05:14 -0500

    Added submodule init and update to setup script

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ cp --backup=numbered --force ~/.vimrc ~/.vimrc
 cd `dirname "$0"`
 CWD=`pwd`
 echo "so $CWD/main.vim" > ~/.vimrc
+
+git submodule init
+git submodule update
+
 echo "Enter your Simplenote username, followed by [ENTER]:"
 read SN_USER
 echo "Enter your Simplenote password, followed by [ENTER]:"
